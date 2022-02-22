98cf2cc0bdd8d44ce40f767162198c403c525c67
seriusanbudi <budiharta_21@live.com>
2022-02-22 23:04:21 +0800

    :sparkles: try to update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h3>Let's start a new journey</h3>
+      <h3>Let's start a new journey updated</h3>
     </div>
   );
 };
